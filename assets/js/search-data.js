f33efcd80812aa396720da4b31491f00e2256e1d
kholl28 <153400372+kholl28@users.noreply.github.com>
2025-01-30 02:55:47 +0000

    Deploying to gh-pages from @ kholl28/kholl28.github.io@32813be1d970e4b4d3d055cc53c6f972cb6db88b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -57,51 +57,11 @@ ninja.data = [{
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-the-impact-of-crime-severity-and-recidivism-on-attitudes-towards-felon-voting-rights",
-          title: 'The Impact of Crime Severity and Recidivism on Attitudes towards Felon Voting Rights...',
+            },},{id: "projects-dacss-758-final-project",
+          title: 'DACSS 758 Final Project',
           description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/602_GroupProjectReport/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+              window.location.href = "/projects/index/";
             },},{
         id: 'social-email',
         title: 'email',
