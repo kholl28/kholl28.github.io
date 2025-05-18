30f426ef33b123c30437ac8cf132ab773dd1aa4d
kholl28 <153400372+kholl28@users.noreply.github.com>
2025-05-18 02:55:20 +0000

    Deploying to gh-pages from @ kholl28/kholl28.github.io@cef1bd3bedfc449bc10deb8ac50d9a95e0613ffc 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -52,8 +52,8 @@ ninja.data = [{
           window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
         
       },
-    },{id: "projects-project-1",
-          title: 'project 1',
+    },{id: "projects-menstrual-period-predictor-ml-model",
+          title: 'Menstrual Period Predictor ML Model',
           description: "with background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
